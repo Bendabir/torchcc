5a2804a5b30e23c146cdd610aee68cc2a3f6e165
Benjamin Riviere <briviere01@gmail.com>
2024-09-21 18:07:08 +0200

    Implement extension build.

--- build.py
+++ build.py
@@ -1,10 +1,33 @@
 """Copyright (c) 2024 Bendabir."""
 
+# mypy: allow-untyped-calls
 from __future__ import annotations
 
+import glob
 from typing import Any
+
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 
 def build(setup_kwargs: dict[str, Any]) -> None:
     """Add specifications to build the CUDA extension."""
-    raise NotImplementedError
+    setup_kwargs.update(
+        {
+            "ext_modules": [
+                CUDAExtension(
+                    # NOTE : Couldn't find a way to build the lib to torchcc._C
+                    #        (or something else) and then import it properly,
+                    #        so the lib file is a top level.
+                    name="_libtorchcc",
+                    sources=[
+                        *glob.glob("csrc/*.cpp"),
+                        *glob.glob("csrc/*.cu"),
+                    ],
+                )
+            ],
+            "cmdclass": {
+                "build_ext": BuildExtension.with_options(no_python_abi_suffix=True)
+            },
+            "zip_safe": False,
+        }
+    )
--- src/torchcc/__init__.py
+++ src/torchcc/__init__.py
@@ -1,1 +1,7 @@
 """Copyright (c) 2024 Bendabir."""
+
+# ruff: noqa: F401, F403
+from __future__ import annotations
+
+import torch
+from _libtorchcc import *
