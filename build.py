14e2838bb35eb170013626bf7fee5f9c768a613a
Benjamin Riviere <briviere01@gmail.com>
2024-09-21 11:42:51 +0200

    Prepare build setup draft.

---
+++ build.py
@@ -0,0 +1,10 @@
+"""Copyright (c) 2024 Bendabir."""
+
+from __future__ import annotations
+
+from typing import Any
+
+
+def build(setup_kwargs: dict[str, Any]) -> None:
+    """Add specifications to build the CUDA extension."""
+    raise NotImplementedError
